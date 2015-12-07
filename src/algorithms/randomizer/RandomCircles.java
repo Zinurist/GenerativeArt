015407dd262cc78a1d7ff6ec98a6495525badc4c
Nicolas Lindae <nicolas@lindae.de>
2015-12-07 16:41:35 +0100

    Add num option to circles

--- src/algorithms/randomizer/RandomCircles.java
+++ src/algorithms/randomizer/RandomCircles.java
@@ -1,11 +1,16 @@
 package algorithms.randomizer;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class RandomCircles extends Randomizer{
+
+    JSpinner num = new JSpinner(new SpinnerNumberModel(30, 0, 100, 15));
+
     @Override
     public void step(Graphics g, int width, int height) {
-        int num1=r.nextInt(90)+10;
+        int num1 = (Integer) num.getValue();
+        num1 = num1==0? r.nextInt(90)+10 : num1;
         int num2, num3;
         for(int i=0; i<num1; i++){
             num1=r.nextInt(50)+50;
@@ -18,6 +23,13 @@ public class RandomCircles extends Randomizer{
             }
             g.drawOval(num2, num3, num1, num1);
         }
+    }
+
+    @Override
+    public java.util.List<Component> getOptionList(){
+        java.util.List<Component> list = super.getOptionList();
+        list.add(num);
+        return list;
     }
 
     @Override
