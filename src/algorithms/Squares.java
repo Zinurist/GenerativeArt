d5fc687bc8c0efba4bdd0c868ca034fa01d47fca
Zinurist <zinu@gmx.net>
2016-01-04 09:54:52 +0100

    Add circles option

--- src/algorithms/Squares.java
+++ src/algorithms/Squares.java
@@ -1,11 +1,30 @@
 package algorithms;
 
+import javax.swing.*;
+import java.util.LinkedList;
+import java.util.List;
 import java.awt.*;
 
 public class Squares extends Algorithm {
+
+
+    JCheckBox circles;
+
+    public Squares(){
+        circles = new JCheckBox("circles");
+        circles.setSelected(false);
+    }
+
     @Override
     public String toString() {
         return "Squares";
+    }
+
+    @Override
+    public java.util.List<Component> getOptionList(){
+        List<Component> list = new LinkedList<Component>();
+        list.add(circles);
+        return list;
     }
 
     @Override
@@ -17,9 +36,16 @@ public class Squares extends Algorithm {
         Graphics g = IMG.createGraphics();
         g.setColor(Color.BLACK);
         while(drawn<IMG.getWidth()){
-            for(int i=0; i<width && drawn<IMG.getWidth(); i++){
-                g.drawRect(xStart-drawn, yStart-drawn,drawn*2,drawn*2);
-                drawn++;
+            if(circles.isSelected()) {
+                for (int i = 0; i < width && drawn < IMG.getWidth(); i++) {
+                    g.drawOval(xStart - drawn, yStart - drawn, drawn * 2, drawn * 2);
+                    drawn++;
+                }
+            }else{
+                for (int i = 0; i < width && drawn < IMG.getWidth(); i++) {
+                    g.drawRect(xStart - drawn, yStart - drawn, drawn * 2, drawn * 2);
+                    drawn++;
+                }
             }
             width++;
             drawn+=width;
