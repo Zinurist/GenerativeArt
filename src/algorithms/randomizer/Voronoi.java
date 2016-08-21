c958ec902b2c55ad854ae5097de972549cb80946
Zinurist <zinu@gmx.net>
2016-08-21 13:50:15 +0200

    Add Voronoi

--- src/algorithms/randomizer/Voronoi.java
+++ src/algorithms/randomizer/Voronoi.java
@@ -22,11 +22,10 @@ public class Voronoi extends Randomizer{
     @Override
     public void step(Graphics g, int width, int height) {
 
-
         g.setColor(Color.BLACK);
         int off = points.size()/2;
         for(int i=0; i<off; i++){
-            g.fillOval(points.get(i)-3, points.get(off+i), 6, 6);
+            g.fillOval(points.get(i)-4, points.get(off+i)-4, 8, 7);//7 instead of 8, because it looks like shit otherwise
         }
 
     }
@@ -40,9 +39,9 @@ public class Voronoi extends Randomizer{
     public void reset() {
         points.ensureCapacity(2*num);
 
-        for(int i=0; i<points.size()/2; i++){
-            points.set(i, r.nextInt(IMG.getWidth()));
-            points.set(num+i, r.nextInt(IMG.getHeight()));
+        for(int i=0; i<num; i++){
+            points.add(i, r.nextInt(IMG.getWidth()));
+            points.add(2*i+1, r.nextInt(IMG.getHeight()));
         }
     }
 
